e5c0ae0896946fe0645ff6bc15d8eff9a7971f65
Cristobal Miranda <cristobal.miranda.tt@gmail.com>
2019-12-03 18:38:32 -0300

    corrected type in BitVector constructor

--- src/BitVector.cpp
+++ src/BitVector.cpp
@@ -6,21 +6,21 @@
 #include <cstring>
 
 namespace succinct_structures{
-  template<typename bv_block_sz>
-  BitVector<bv_block_sz>::BitVector(uint bv_size) : bv_size(bv_size) {
+  template<typename bv_block_t>
+  BitVector<bv_block_t>::BitVector(uint bv_size) : bv_size(bv_size) {
     remainder = bv_size % bv_block_bits;
     num_of_blocks = remainder == 0 ? bv_size / bv_block_bits : bv_size / bv_block_bits + 1;
-    container = std::make_unique<uint[]>(num_of_blocks);
+    container = std::make_unique<bv_block_t[]>(num_of_blocks);
     clearContainer();
   }
 
-  template<typename bv_block_sz>
-  uint BitVector<bv_block_sz>::getBVSize() const {
+  template<typename bv_block_t>
+  uint BitVector<bv_block_t>::getBVSize() const {
     return bv_size;
   }
 
-  template<typename bv_block_sz>
-  void BitVector<bv_block_sz>::clearContainer() {
+  template<typename bv_block_t>
+  void BitVector<bv_block_t>::clearContainer() {
     std::memset(container.get(), 0, num_of_blocks);
   }
 
